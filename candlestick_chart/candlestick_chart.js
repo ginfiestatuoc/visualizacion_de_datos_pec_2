f002c7792c167559e92f90f565197a00f9296807
ginfiestatuoc <150593472+ginfiestatuoc@users.noreply.github.com>
2023-11-14 16:08:51 +0100

    Add files via upload

---
+++ candlestick_chart/candlestick_chart.js
@@ -0,0 +1,54 @@
+// SOURCE: https://codepen.io/team/amcharts/pen/dmEzLy
+import {data} from "./data.js";
+
+am4core.useTheme(am4themes_animated);
+window.candle_data = data
+var chart = am4core.create("candlestick_chart_container", am4charts.XYChart);
+chart.paddingRight = 20;
+
+chart.dateFormatter.inputDateFormat = "YYYY-MM-dd";
+
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.grid.template.location = 0;
+
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.tooltip.disabled = true;
+
+var series = chart.series.push(new am4charts.CandlestickSeries());
+series.dataFields.dateX = "date";
+series.dataFields.valueY = "close";
+series.dataFields.openValueY = "open";
+series.dataFields.lowValueY = "low";
+series.dataFields.highValueY = "high";
+series.simplifiedProcessing = true;
+series.tooltipText = "Open:${openValueY.value}\nLow:${lowValueY.value}\nHigh:${highValueY.value}\nClose:${valueY.value}";
+
+// important!
+// candlestick series colors are set in states. 
+// series.riseFromOpenState.properties.fill = am4core.color("#00ff00");
+// series.dropFromOpenState.properties.fill = am4core.color("#FF0000");
+// series.riseFromOpenState.properties.stroke = am4core.color("#00ff00");
+// series.dropFromOpenState.properties.stroke = am4core.color("#FF0000");
+
+series.riseFromPreviousState.properties.fillOpacity = 1;
+series.dropFromPreviousState.properties.fillOpacity = 0;
+
+chart.cursor = new am4charts.XYCursor();
+
+// a separate series for scrollbar
+var lineSeries = chart.series.push(new am4charts.LineSeries());
+lineSeries.dataFields.dateX = "date";
+lineSeries.dataFields.valueY = "close";
+// need to set on default state, as initially series is "show"
+lineSeries.defaultState.properties.visible = false;
+
+// hide from legend too (in case there is one)
+lineSeries.hiddenInLegend = true;
+lineSeries.fillOpacity = 0.5;
+lineSeries.strokeOpacity = 0.5;
+
+var scrollbarX = new am4charts.XYChartScrollbar();
+scrollbarX.series.push(lineSeries);
+chart.scrollbarX = scrollbarX;
+
+chart.data = data;
---
+++ spiral_chart/spiral_chart.js
@@ -0,0 +1,96 @@
+/* SOURCE: https://www.amcharts.com/demos-v4/spiral-chart-v4/ */
+
+import {data} from "./data.js";
+
+am4core.useTheme(am4themes_animated);
+
+
+let chart = am4core.create("spiralplot_container", am4plugins_timeline.SpiralChart);
+chart.levelCount = 3;
+chart.curveContainer.padding(40, 40, 40, 40);
+
+chart.data = data;
+
+let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.grid.template.location = 0;
+dateAxis.renderer.line.disabled = true;
+dateAxis.cursorTooltipEnabled = false;
+dateAxis.renderer.minGridDistance = 30;
+dateAxis.baseInterval.timeUnit = "year";
+dateAxis.tooltipDateFormat = "yyyy";
+dateAxis.minZoomCount = 5;
+dateAxis.dateFormatter = new am4core.DateFormatter();
+dateAxis.dateFormatter.dateFormat = "yyyy";
+
+window.xaxis = dateAxis;
+
+let labelTemplate = dateAxis.renderer.labels.template;
+labelTemplate.fontSize = 13;
+labelTemplate.verticalCenter = "middle";
+labelTemplate.horizontalCenter = "middle";
+labelTemplate.padding(3, 3, 3, 3);
+
+labelTemplate.adapter.add("rotation", function (rotation, target) {
+    let value = target.dataItem.value;
+    let endvalue = target.dataItem.endValue;
+
+    let position = dateAxis.valueToPosition(value + (endvalue - value) / 2);
+
+    let angle = dateAxis.renderer.positionToAngle(position);
+
+    return angle;
+})
+
+let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.tooltip.disabled = true;
+valueAxis.renderer.innerRadius = -25;
+valueAxis.renderer.radius = 25;
+valueAxis.renderer.minGridDistance = 15;
+valueAxis.renderer.axisLocation = 1;
+
+
+let valueAxisLabelTemplate = valueAxis.renderer.labels.template;
+valueAxisLabelTemplate.rotation = - 90;
+valueAxisLabelTemplate.fontSize = 10;
+valueAxisLabelTemplate.horizontalCenter = "left";
+
+
+valueAxisLabelTemplate.adapter.add("rotation", function (rotation, target) {
+    return dateAxis.renderer.positionToAngle(1) - 90;
+})
+
+chart.seriesContainer.zIndex = -1;
+
+let series = chart.series.push(new am4plugins_timeline.CurveColumnSeries());
+series.dataFields.dateX = "year";
+series.dataFields.valueY = "value";
+series.tooltipText = "{valueY}";
+series.tooltip.pointerOrientation = "vertical";
+series.tooltip.background.fillOpacity = 0.7;
+series.strokeWidth = 2;
+series.heatRules.push({
+    "target": series.columns.template,
+    "property": "stroke",
+    "min": am4core.color("#005f96"),
+    "max": am4core.color("#73EDFF"),
+    "dataField": "valueY"
+});
+series.heatRules.push({
+    "target": series.columns.template,
+    "property": "fill",
+    "min": am4core.color("#005f96"),
+    "max": am4core.color("#73EDFF"),
+    "dataField": "valueY"
+});
+
+chart.dateFormatter.dateFormat = "yyyy"
+window.chart = chart;
+window.series = series;
+chart.cursor = new am4plugins_timeline.CurveCursor();
+chart.cursor.xAxis = dateAxis;
+chart.cursor.yAxis = valueAxis;
+chart.cursor.lineY.disabled = true;
+
+chart.scrollbarX = new am4core.Scrollbar();
+chart.scrollbarX.width = am4core.percent(80);
+chart.scrollbarX.align = "center";
